7b0b8349c69024c85058c50f6e4dc1ae5a379d95
AnuragV314 <vermagup01@gmail.com>
2023-10-10 00:24:21 +0530

    DSA-JAVA

--- Arrays/ArrayListA.java
+++ Arrays/ArrayListA.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// JAVA - Search
 // ---------------------------------------------------------------
 // class ArrayListA
 // { 
@@ -22,17 +23,71 @@ import java.util.*;
 // }
 
 // ---------------------------------------------------------------
-// JAVA
-class ArrayListA {
-    public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        arr.add(4);
-        arr.add(5);
-        arr.add(6);
-        arr.add(7);
-        arr.add(8);
-        arr.add(10);
+// JAVA - With function - indexOf()
 
-        System.out.println(arr.indexOf(7));
+// class ArrayListA {
+//     public static void main(String[] args) {
+//         ArrayList<Integer> arr = new ArrayList<Integer>();
+//         arr.add(4);
+//         arr.add(5);
+//         arr.add(6);
+//         arr.add(7);
+//         arr.add(8);
+//         arr.add(10);
+
+//         System.out.println(arr.indexOf(7));
+//     }
+// }
+
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
+// JAVA - Insert  
+// -simple
+
+class ArrayListA {
+
+    static int insert(int arr[], int n, int x, int cap, int pos) {
+        if (n == cap)
+            return n;
+
+        int idx = pos - 1;
+
+        for (int i = n - 1; i >= idx; i--) {
+            arr[i + 1] = arr[i];
+        }
+
+        arr[idx] = x;
+
+        return n + 1;
     }
+
+    public static void main(String args[]) {
+        int arr[] = new int[5], cap = 5, n = 3;
+
+        arr[0] = 5;
+        arr[1] = 10;
+        arr[2] = 20;
+
+        System.out.println("Before Insertion");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+        int x = 7, pos = 2;
+
+        n = insert(arr, n, x, cap, pos);
+
+        System.out.println("After Insertion");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
 }
+
+// ---------------------------------------------------------------
+// - with Function - add(position , value)
