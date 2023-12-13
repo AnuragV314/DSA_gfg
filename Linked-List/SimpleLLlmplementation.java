32cd989e9edb9ae632ed332af172e420c16efeff
AnuragV314 <vermagup01@gmail.com>
2023-12-13 08:37:46 +0530

    DSA-JAVA

---
+++ Linked-List/SimpleLLlmplementation.java
@@ -0,0 +1,46 @@
+/**
+ * Simple Linked List Implementation
+ */
+
+// class Node {
+//     int data;
+//     Node next;
+
+//     Node(int item) {
+//         data = item;
+//         next = null;
+//     }
+// }
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+public class SimpleLLlmplementation {
+
+    static void travalLL(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.data +" ");
+        travalLL(head.next);
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1, new Node(2, new Node(3, null)));
+        // while (head.next != null) {
+        //     System.out.print(head.data + " ");
+        //     head = head.next;
+        // }
+        // System.out.print(head.data);
+        // System.out.println();
+
+        travalLL(head);
+    }
+}
